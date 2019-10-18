9ec980aacbed7662faa2fa409f7524be1c174cd0
Robert Danielsson <robert.danielsson@yh.nackademin.se>
2019-10-18 10:52:37 +0200

    c1 finished

---
+++ AlloyAdvanced/Features/Favorites/FavoriteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlloyAdvanced.Features.Favorites
+{
+    public class FavoriteViewModel
+    {
+        public List<Favorite> Favorites { get; set; }
+        public ContentReference CurrentPageContentReference { get; set; }
+    }
+}
