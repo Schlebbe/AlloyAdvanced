f3542383939c5fe891dcd6f8e17d1a4f3e932315
Robert Danielsson <robert.danielsson@yh.nackademin.se>
2019-10-20 13:19:27 +0200

    c3 completed

---
+++ NorthwindWebApi/Controllers/ShipperFormController.cs
@@ -0,0 +1,27 @@
+﻿using NorthwindWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NorthwindWebApi.Controllers
+{
+    public class ShipperFormController : ApiController
+    {
+        public string Get()
+        {
+            return "ShipperFormController says, 'Hi!'";
+        }
+        public void Post(Shipper shipper)
+        {
+            string add_data = System.Web.HttpContext.Current
+            .Server.MapPath(@"~\App_Data");
+            string path = Path.Combine(add_data,
+            $"shipperform_{DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss")}");
+            File.WriteAllText(path, $"{shipper.ShipperID}: {shipper.CompanyName}");
+        }
+    }
+}
